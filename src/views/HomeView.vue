1bc6d150c4300d70b81311de6495a54db3c50445
金名 <2269009160@qq.com>
2022-10-24 10:31:57 +0800

    '实现进入页面小说自动排版'

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="home">
     <div class="text">
-      <div ref="content" class="content" v-html="content" ></div>
-      <div class="content"  v-for="(r,i) in renderList" :key="i" v-show="i!=0">
-      <div class="list" v-for="(p,i) in r" :key="i" v-show="i !=0">{{p}}</div>
+      <div
+        ref="content"
+        class="content"
+        v-html="content"
+        @click="PageList"
+      ></div>
+      <div
+        class="content"
+        v-for="(r, i) in renderList"
+        :key="i"
+        v-show="i != 0"
+      >
+        <div class="list" v-for="(p, i) in r" :key="i" v-show="i != 0">
+          {{ p }}
+        </div>
+      </div>
     </div>
-    </div>
-    
-    <button @click="PageList">1111111111111</button>
-    
+
+    <!-- <button @click="PageList">1111111111111</button> -->
   </div>
 </template>
 
@@ -18,74 +29,78 @@ export default {
     return {
       content: null,
       // itemList:[],
-      index:0,
-      nextIndex:0,
-      arrNum:1,
-      count:0,
-      renderList:[]
+      index: 0,
+      nextIndex: 0,
+      arrNum: 1,
+      count: 0,
+      renderList: [],
+      str5: [],
+      fontSize: 12,
+      heightList: [],
+      textList: [],
     };
   },
 
   computed: {
-    itemHeightList() {
-      let p = document.querySelectorAll("p");
-      let span = document.querySelector("span");
-      let heightList = [];
-      heightList.push(span.offsetHeight+6);
-      p.forEach((i) => {
-        heightList.push(i.offsetHeight);
-      });
-      return heightList;
-    },
-
-    itemTextList() {
-      let p = document.querySelectorAll("p");
-      let span = document.querySelector("span");
-      let textList = [];
-      textList.push(span.textContent);
-      p.forEach((i) => {
-        textList.push(i.textContent);
-      });
-      return textList;
-    },
-
     viewHeight() {
       return this.$refs.content.offsetHeight;
     },
+    viewWidth() {
+      return this.$refs.content.offsetWidth;
+    },
   },
 
-  // updated(){
-
-  // }
-
   methods: {
-    PageList(){
-      for(;;){
-        for(let i= this.index;i<this.itemHeightList.length;i++){
-          this.count +=this.itemHeightList[i]
-          console.log(this.count,this.viewHeight*this.arrNum);
-          if(this.count>=this.viewHeight*this.arrNum){
-            this.nextIndex=i
-           break
+    replaceText() {
+      let str = this.content.replace(
+        /<div class="g-book"><div class="m-mb"><div class="m-content">/g,
+        ""
+      );
+      let str1 = str.replace(/<h1><span>/g, "<p>");
+      let str2 = str1.replace(/<\/span><\/h1>/g, "</p>");
+      let str3 = str2.replace(/<p>/g, "");
+      let str4 = str3.replace(
+        /<div id="book-bottom"><\/div><\/div><\/div><\/div>/g,
+        ""
+      );
+      this.str5 = str4.split("</p>");
+      // console.log(this.str5);
+      this.str5.forEach((i) => {
+        this.heightList.push({
+          width: i.length * this.fontSize,
+          height:
+            Math.ceil((i.length * this.fontSize) / this.viewWidth) *
+            this.fontSize,
+        });
+        this.textList.push(i);
+      });
+    },
+
+    PageList() {
+      for (;;) {
+        for (let i = this.index; i < this.heightList.length; i++) {
+          this.count += this.heightList[i].height + 40;
+          if (this.count >= this.viewHeight * this.arrNum) {
+            this.nextIndex = i;
+            break;
           }
         }
-        
-        if(this.index==this.nextIndex){
-           this.renderList.push(this.itemTextList.slice(this.nextIndex,this.itemTextList.length));
-           break
-        }else{
-          this.renderList.push(this.itemTextList.slice(this.index, this.nextIndex));
+
+        if (this.index == this.nextIndex) {
+          this.renderList.push(
+            this.textList.slice(this.nextIndex, this.textList.length)
+          );
+          break;
+        } else {
+          this.renderList.push(this.textList.slice(this.index, this.nextIndex));
         }
-         console.log(this.renderList);
-        this.index=this.nextIndex
-        this.arrNum=this.arrNum+1
+        //  console.log(this.renderList);
+        this.index = this.nextIndex;
+        this.arrNum = this.arrNum + 1;
       }
-        console.log(this.renderList);
     },
     get() {
-      //  console.log(this.itemHeightList);
-      //  let p = document.querySelectorAll("p");
-      // console.log(p[0].textContent);
+      console.log(1);
     },
     getData() {
       this.$axios
@@ -94,29 +109,34 @@ export default {
         )
         .then(({ data }) => {
           this.content = data.data.content;
-          // console.log(data.data);
+          this.replaceText();
+          this.PageList();
+          console.log(this.renderList);
+          // console.log(this.heightList,this.viewWidth);
         });
     },
   },
+  created() {},
   mounted() {
     this.getData();
-    // this.PageList();
+
+    //  console.log(this.heightList,this.textList);
   },
 };
 </script>
 
 <style lang="scss" >
 h1 {
-  span{
-    font-weight:700;
+  span {
+    font-weight: 700;
     font-size: 24px;
   }
 }
-.home{
+.home {
   width: 100vw;
   height: 100vh;
 }
-.text{
+.text {
   width: 10000px;
   display: flex;
   flex-direction: row;
@@ -124,19 +144,18 @@ h1 {
 }
 .content {
   width: 100vw;
-  height:100vh;
+  height: 100vh;
   overflow: hidden;
-  p{
+  p {
     padding: 20px 10px;
     // font-size: 21px;
     font-size: 12px;
   }
 }
 
-  .list{
-    padding: 20px 10px;
-    // font-size: 21px;
-    font-size: 12px;
-  }
-
+.list {
+  padding: 20px 10px;
+  // font-size: 21px;
+  font-size: 12px;
+}
 </style>
